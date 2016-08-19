3b17ca7193958c71396e34570b17e5e684c84095
Andrew <andrew.vinocur@gmail.com>
2016-08-19 16:21:23 -0700

    board has correct CSS, click counter works

--- js/app.js
+++ js/app.js
@@ -2,5 +2,27 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+console.log("Sanity Check");
+$('.box').on('click', function() {
+  console.log("Click on a box");
+    $('#output').html(function(i, val) {
+      return val*1+1
+    }
+  );
+})
+})
 
-});
+
+
+// $('.box').on("click", turnCounter(){
+//   var turnCounter = function(){
+//     for(var i = 0; i <= 9; i++){
+//       console.log(i);
+//     }
+//   }
+// }
+
+
+// $('.box').on("click", placeSymbol(){
+//
+// }
