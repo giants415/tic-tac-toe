12f7886759382511dd8d30efc085eb432b06e68a
Andrew <andrew.vinocur@gmail.com>
2016-08-21 21:40:57 -0700

    working tic tac toe game with all win conditions

--- js/app.js
+++ js/app.js
@@ -1,36 +1,94 @@
 // wait for the DOM to finish loading
-var currentState = [];
+var currentState = [-1,-1,-1,
+                    -1,-1,-1,
+                    -1,-1,-1];
 var currentTurn = 0;
+var checkHorizontal = function(index){
+  var findPlayer = currentState[index];
+    console.log("looking for player " + findPlayer);
+    if (index == 0 || index == 1 || index == 2) {
+        if (findPlayer == currentState[0] && findPlayer == currentState[1] && findPlayer == currentState[2]) {
+          return true;
+        }
+    } else if (index == 3 || index == 4 || index == 5) {
+      if (findPlayer == currentState[3] && findPlayer == currentState[4] && findPlayer == currentState[5]) {
+        return true;
+      }
+    } else {
+      if (findPlayer == currentState[6] && findPlayer == currentState[7] && findPlayer == currentState[8]) {
+        return true;
+      }
+    }
+    return false;
+};
+
+var checkVertical = function(index){
+  var findPlayer = currentState[index];
+    if (index == 0 || index == 3 || index == 6) {
+        if (findPlayer == currentState[0] && findPlayer == currentState[3] && findPlayer == currentState[6]) {
+          return true;
+        }
+    } else if (index == 1 || index == 4 || index == 7) {
+      if (findPlayer == currentState[1] && findPlayer == currentState[4] && findPlayer == currentState[7]) {
+        return true;
+      }
+    } else {
+      if (findPlayer == currentState[2] && findPlayer == currentState[5] && findPlayer == currentState[8]) {
+        return true;
+      }
+    }
+    return false;
+};
+
+var checkDiagonal = function(index){
+  var findPlayer = currentState[index];
+    if (index == 0 || index == 4 || index == 8) {
+        if (findPlayer == currentState[0] && findPlayer == currentState[4] && findPlayer == currentState[8]) {
+          return true;
+        }
+    } else if (index == 2 || index == 4 || index == 6) {
+      if (findPlayer == currentState[2] && findPlayer == currentState[4] && findPlayer == currentState[6]) {
+        return true;
+      }
+    }
+  return false;
+};
+
 // all code to manipulate the DOM
 // goes inside this function
 $(document).ready(function() {
 console.log("Sanity Check");
 $('.box').on('click', function() {
-var turnCounter; // var turnCounter = val+1; put the turnCounter variable here so it stays w/in the scope and can access the val
-var clickedBox = $(this);
   console.log("Click on " + this.id);
-  if (currentTurn%2 == 0) {
-    $(this).append("<img src=\"x.png\"></img>");
-    turnChange();
+  var index = this.id.replace('box', '');
+  currentState[index - 1] = currentTurn;
+  if (currentTurn == 0) {
+    currentTurn = 1;
+    $(this).append("<img class=\"gamePice\" src=\"x.png\"></img>");
   } else {
-    $(this).append("<img src=\"o.png\"></img>");
-    turnChange();
+    currentTurn = 0;
+    $(this).append("<img  class=\"gamePice\" src=\"o.png\"></img>");
   }
-    $('#output').html(function(i, turnCounter) {
-      return turnCounter*1+1;
-      })
-    }
-  );
-})
+  $(this).unbind( "click" );
+  //now we check for playr victories
+  var horizontailWin = checkHorizontal(index-1);
+  var vertWin = checkVertical(index-1);
+  var diagWin = checkDiagonal(index-1);
+  var win = horizontailWin || vertWin || diagWin;
+  if(win) {
+    alert("We have a winner!");
+  }
+  console.log("win = " + horizontailWin);
+}); //end of click function
+}); //end of dom ready
 
-var turnChange = function() {
-  for (var i = 0; i <= 8; i++) {
-    var currentTurn = currentTurn + 1;
-    console.log(currentTurn);
-  }
-}
 
 ///var clickedBox = $(this); use this to keep track of EXACTLY which #'ed box was clicked
+
+// ORIGINAL TURN COUNTER CODE
+// $('#output').html(function(i, turnCounter) {
+//   return turnCounter*1+1;
+//   })
 
 
 // $('.box').on("click", turnCounter(){
